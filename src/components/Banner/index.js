9752119e8fc3d6976ace1db9adc365c75a187afd
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-01-01 15:55:29 -0300

    Added Banner

---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Banner = () => {
+  return (
+    <div className='mv-banner'>
+      <div className='container-fluid'>
+        <h1 className='mv-colors__text__white'>CONHEÇA NOSSOS PERSONAGENS</h1>
+      </div>
+    </div>
+  )
+}
